08a1015151443a16b786a62d1da363fe016e54f0
Neda <nedachangizi@gmail.com>
2022-11-22 15:38:02 -0800

    change text

--- scrips/script.js
+++ scrips/script.js
@@ -1,16 +1,22 @@
-const clickButton = document.querySelector('.click-me');
+const clickButton = document.querySelector(".click-me");
 // console.log(clickButton);
-function clickHandler()
-{
-    alert("You clicked me!");
-    // clickButton.removeEventListener("click", clickHandler);
+function clickHandler() {
+  alert("You clicked me!");
+  // clickButton.removeEventListener("click", clickHandler);
 }
-clickButton.addEventListener("click", clickHandler, {once:true} );
+clickButton.addEventListener("click", clickHandler, { once: true });
 
-function changeBGPink()
-{
-    // document.body.style.backgroundColor = "pink";
-    document.body.classList.add("pinkBG");
+function changeBGPink() {
+  // document.body.style.backgroundColor = "pink";
+  document.body.classList.add("pinkBG");
 }
-clickButton.addEventListener("click", changeBGPink)
+clickButton.addEventListener("click", changeBGPink);
 
+function changeText() {
+  if (clickButton.textContent === "Click Me!") {
+    clickButton.textContent = "clicked";
+  } else if (clickButton.textContent === "clicked") {
+    clickButton.textContent = "Click Me!";
+  }
+}
+clickButton.addEventListener("click", changeText);
