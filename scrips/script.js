bd03fa781b4ac71c5e631f2f816b312501915c0b
Neda <nedachangizi@gmail.com>
2022-11-22 14:54:41 -0800

    remove event listener

--- scrips/script.js
+++ scrips/script.js
@@ -2,6 +2,8 @@ const clickButton = document.querySelector('.click-me');
 // console.log(clickButton);
 function clickHandler()
 {
-    alert("You clicked me!")
+    alert("You clicked me!");
+    // clickButton.removeEventListener("click", clickHandler);
 }
-clickButton.addEventListener("click", clickHandler);
+clickButton.addEventListener("click", clickHandler, {once:true} );
+
